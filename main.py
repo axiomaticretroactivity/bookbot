93417e7e60b0eb5a1cfc9d76f92f99108bbf7ab5
axiomaticretroactivity <axiomaticretroactivity@gmail.com>
2025-10-27 00:52:54 -0500

    modified main.py to accept a path in CLI

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from stats import get_word_count
 from stats import count_characters
 from stats import report_character_counts
@@ -7,17 +9,21 @@ def get_book_text(path_to_file):
         return f.read()
     
 def main():
-    frankenstein_path = "books/frankenstein.txt"
-    frankenstein = get_book_text(frankenstein_path)
-    word_count_frankenstein = get_word_count(frankenstein)
-    character_counts_frankenstein = report_character_counts(count_characters(frankenstein))
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    book_path = str(sys.argv[1])
+    book = get_book_text(book_path)
+    book_word_count = get_word_count(book)
+    book_character_counts = report_character_counts(count_characters(book))
 
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {frankenstein_path}...")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
-    print(f"Found {word_count_frankenstein} total words")
+    print(f"Found {book_word_count} total words")
     print("--------- Character Count -------")
-    for count in character_counts_frankenstein:
+    for count in book_character_counts:
         print(f"{count["char"]}: {count["num"]}")
     
 
